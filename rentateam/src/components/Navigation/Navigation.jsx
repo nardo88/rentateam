84e3554f74fc282c14e7c0a50b60156a7dcdc3f8
Maxim Kuznecov <maksimkuznetsov@topverstka.ru>
2021-06-30 23:08:00 +0300

    1

--- rentateam/src/components/Navigation/Navigation.jsx
+++ rentateam/src/components/Navigation/Navigation.jsx
@@ -7,6 +7,9 @@ const Navigation = ({ data }) => {
         <div className="navigation">
             <div className="container">
                 <div className="navigation__wrapper">
+                    <div className="burger">
+                        <button className="burger__btn burger__btn--open"><span></span></button>
+                    </div>
                     <ul className="navigation__list">
                         {
                             data.categoryes.map(item => <NavigationItem key={item.id} item={item}/>)
